95930acef90450e60bb63f17b81409a4356f7478
Lucas Ly <lucasly168@gmail.com>
2022-09-16 16:50:12 +0000

    Pushed from Locofy Builder

---
+++ pages/index.tsx
@@ -0,0 +1,218 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+
+const LandingPage: NextPage = () => {
+  return (
+    <div className={styles.landingPageDiv}>
+      <div className={styles.block6Div}>
+        <img
+          className={styles.bgGradientImgIcon}
+          alt=""
+          src="../bggradientimg@2x.png"
+        />
+        <img className={styles.bgImgIcon} alt="" src="../bgimg.svg" />
+        <div className={styles.textBlockDiv}>
+          <div className={styles.textDiv}>
+            <div className={styles.takeYourFirstStepIntoSaf}>
+              <p className={styles.takeYourFirst}>{`Take your first step `}</p>
+              <p className={styles.takeYourFirst}>{`into safe, secure `}</p>
+              <p className={styles.cryptoInvesting}>crypto investing</p>
+            </div>
+            <div className={styles.separatedTheyLiveInBookmar}>
+              Separated they live in Bookmarks right at the coast of the famous
+              Semantics, large language ocean Separated they live in Bookmarks
+              right at the coast.
+            </div>
+          </div>
+          <div className={styles.bttnDiv}>
+            <div className={styles.textDiv1}>Get Started</div>
+          </div>
+        </div>
+        <img className={styles.imgIcon} alt="" src="../img.svg" />
+      </div>
+      <div className={styles.block5Div}>
+        <img
+          className={styles.bgGradientImgIcon1}
+          alt=""
+          src="../bggradientimg1@2x.png"
+        />
+        <div className={styles.tableDiv}>
+          <div className={styles.rowDiv}>
+            <b className={styles.serverSpeedsB}>Server speeds</b>
+            <b className={styles.server1B}>Server 1</b>
+            <b className={styles.fastB}>Fast</b>
+            <b className={styles.b}>+1.68%</b>
+            <img
+              className={styles.smallGraphIcon}
+              alt=""
+              src="../smallgraph.svg"
+            />
+            <button className={styles.bttnButton}>
+              <div className={styles.textDiv2}>Server Information</div>
+              <img className={styles.iconarrow} alt="" src="../iconarrow.svg" />
+            </button>
+          </div>
+          <div className={styles.lineDiv} />
+          <div className={styles.rowDiv}>
+            <b className={styles.serverSpeedsB}>Server uptime</b>
+            <b className={styles.server1B}>Server 2</b>
+            <b className={styles.fastB}>100%</b>
+            <b className={styles.b}>+4.36%</b>
+            <img
+              className={styles.smallGraphIcon}
+              alt=""
+              src="../smallgraph1.svg"
+            />
+            <div className={styles.bttnDiv1}>
+              <div className={styles.textDiv3}>Server information</div>
+              <img className={styles.iconarrow} alt="" src="../iconarrow.svg" />
+            </div>
+          </div>
+          <div className={styles.lineDiv} />
+          <div className={styles.rowDiv2}>
+            <b className={styles.serverSpeedsB}>Cardano</b>
+            <b className={styles.server1B}>ADA</b>
+            <b className={styles.fastB}>$1.88</b>
+            <b className={styles.b}>+3.43%</b>
+            <img
+              className={styles.smallGraphIcon2}
+              alt=""
+              src="../smallgraph2.svg"
+            />
+            <div className={styles.bttnDiv1}>
+              <div className={styles.textDiv3}>Trade Now</div>
+              <img className={styles.iconarrow} alt="" src="../iconarrow.svg" />
+            </div>
+          </div>
+          <div className={styles.lineDiv} />
+          <div className={styles.rowDiv}>
+            <b className={styles.serverSpeedsB}>Usage</b>
+            <b className={styles.server1B}>Server 3</b>
+            <b className={styles.fastB}>140%</b>
+            <b className={styles.moderateB}>Moderate</b>
+            <img
+              className={styles.smallGraphIcon3}
+              alt=""
+              src="../smallgraph3.svg"
+            />
+            <button className={styles.bttnButton1}>
+              <div className={styles.textDiv2}>Server information</div>
+              <img className={styles.iconarrow} alt="" src="../iconarrow.svg" />
+            </button>
+          </div>
+          <div className={styles.lineDiv} />
+          <div className={styles.rowDiv}>
+            <b className={styles.serverSpeedsB}>Users</b>
+            <b className={styles.server1B}>Server 4</b>
+            <b className={styles.fastB}>1000</b>
+            <b className={styles.b}>+7.56%</b>
+            <img
+              className={styles.smallGraphIcon}
+              alt=""
+              src="../smallgraph1.svg"
+            />
+            <div className={styles.bttnDiv1}>
+              <div className={styles.textDiv3}>Server information</div>
+              <img className={styles.iconarrow} alt="" src="../iconarrow.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.bttnDiv4}>
+        <div className={styles.textDiv7}>About Us</div>
+      </div>
+      <img className={styles.iconarrow5} alt="" src="../iconarrow5.svg" />
+      <div className={styles.headerDiv}>
+        <div className={styles.logoDiv} />
+        <div className={styles.menuDiv}>
+          <button className={styles.bttnButton2}>
+            <button className={styles.libraryButton}>Library</button>
+          </button>
+          <div className={styles.bttnDiv5}>
+            <button className={styles.libraryButton}>Courses</button>
+          </div>
+          <div className={styles.bttnDiv6}>
+            <button className={styles.libraryButton}>Forum</button>
+          </div>
+          <div className={styles.bttnDiv7}>
+            <button className={styles.libraryButton}>Organizations</button>
+          </div>
+          <div className={styles.bttnDiv8}>
+            <button className={styles.libraryButton}>Support</button>
+          </div>
+        </div>
+        <img
+          className={styles.localtechnerds1Icon}
+          alt=""
+          src="../localtechnerds-1@2x.png"
+        />
+        <button className={styles.bttnButton3}>
+          <div className={styles.textDiv8}>Sign in</div>
+        </button>
+      </div>
+      <button className={styles.bttnButton4}>
+        <div className={styles.textDiv9}>Sign up</div>
+      </button>
+      <img className={styles.ellipseIcon} alt="" src="../ellipse-77.svg" />
+      <img className={styles.ellipseIcon1} alt="" src="../ellipse-84.svg" />
+      <img className={styles.ellipseIcon2} alt="" src="../ellipse-85.svg" />
+      <img className={styles.ellipseIcon3} alt="" src="../ellipse-86.svg" />
+      <img className={styles.ellipseIcon4} alt="" src="../ellipse-78.svg" />
+      <img className={styles.ellipseIcon5} alt="" src="../ellipse-81.svg" />
+      <img className={styles.ellipseIcon6} alt="" src="../ellipse-83.svg" />
+      <img className={styles.ellipseIcon7} alt="" src="../ellipse-82.svg" />
+      <img className={styles.ellipseIcon8} alt="" src="../ellipse-79.svg" />
+      <img className={styles.ellipseIcon9} alt="" src="../ellipse-80.svg" />
+      <img className={styles.ellipseIcon10} alt="" src="../ellipse-87.svg" />
+      <img className={styles.ellipseIcon11} alt="" src="../ellipse-88.svg" />
+      <img className={styles.ellipseIcon12} alt="" src="../ellipse-89.svg" />
+      <img className={styles.textureIcon} alt="" src="../texture@2x.png" />
+      <div className={styles.localTechNerdsWhereCollabora}>
+        <p className={styles.takeYourFirst}>
+          <span>LocalTechNerds</span>
+        </p>
+        <p className={styles.whereCollaborationP}>
+          <span>Where collaboration</span>
+        </p>
+        <p className={styles.takeYourFirst}>
+          <span>and learning</span>
+        </p>
+        <p className={styles.takeYourFirst}>
+          <span>meets.</span>
+        </p>
+      </div>
+      <img className={styles.starImgIcon} alt="" src="../starimg.svg" />
+      <img className={styles.starImgIcon1} alt="" src="../starimg1.svg" />
+      <img className={styles.starImgIcon2} alt="" src="../starimg2.svg" />
+      <div className={styles.withASingleGestureGainAc}>
+        With a single gesture, gain access to all of the developer's needs on a
+        single platform for free.
+      </div>
+      <div className={styles.weGuaranteeYouTheFastestS}>
+        <p className={styles.takeYourFirst}>
+          We guarantee you the fastest servers for optimal experience with 300%
+          of reusable energy.
+        </p>
+        <p className={styles.cryptoInvesting}>
+          No longer having to hurt the environment for just running a web
+          application!
+        </p>
+      </div>
+      <button className={styles.bttnButton5}>
+        <div className={styles.textDiv10}>Get started</div>
+      </button>
+      <div className={styles.rectangleDiv} />
+      <img className={styles.image1Icon} alt="" src="../image-1@2x.png" />
+      <img className={styles.image2Icon} alt="" src="../image-2@2x.png" />
+      <img className={styles.image3Icon} alt="" src="../image-3@2x.png" />
+      <div className={styles.allCombinedIntoOnePlatform}>
+        All combined into one platform.
+      </div>
+      <div className={styles.weOfferOptimizedServersFor}>
+        We offer optimized servers for your needs.
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
